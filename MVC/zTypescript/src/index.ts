abea8c9ac4d3dd1856933be808615e30a13f3f34
Nayan Kumar <naayaankumar@gmail.com>
2022-12-02 22:55:30 +0530

    added  plain typescript

---
+++ MVC/zTypescript/src/index.ts
@@ -0,0 +1,10 @@
+// import express from "express"
+
+// const app = express()
+
+
+// app.listen(8080,()=>{
+//     console.log("listening on http://localhost:8080")
+// })
+const x :string = "Hello world"
+console.log(x)
